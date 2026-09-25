6ed55341df84ba8d5516ff8987d19b141ecc6128
KP23545756 <162136507+KP23545756@users.noreply.github.com>
2026-09-25 21:03:25 +0200

    Mediator

--- src/CentralDispatchMediator.cpp
+++ src/CentralDispatchMediator.cpp
@@ -1,21 +1,39 @@
 #include "CentralDispatchMediator.h"
 #include "ResponseUnit.h"
 #include "DoorControlInterface.h"
+#include <algorithm>
+#include <iostream>
 
-CentralDispatchMediator::CentralDispatchMediator(DoorControlInterface* d) : doors(d) {
-	// TODO - implement CentralDispatchMediator::CentralDispatchMediator
-}
+CentralDispatchMediator::CentralDispatchMediator(DoorControlInterface* d) : doors(d) {}
 
-CentralDispatchMediator::~CentralDispatchMediator() {
-	// TODO - implement CentralDispatchMediator::~CentralDispatchMediator
-}
+CentralDispatchMediator::~CentralDispatchMediator() {}
 
 void CentralDispatchMediator::registerColleague(ResponseUnit* unit) {
-	// TODO - implement CentralDispatchMediator::registerColleague
-	throw "Not yet implemented";
+	if (!unit) {
+        return;
+    }
+    bool alreadyRegistered = std::find(colleagues.begin(), colleagues.end(), unit) != colleagues.end();
+    if (!alreadyRegistered) {
+        colleagues.push_back(unit);
+    }
 }
 
 void CentralDispatchMediator::notify(ResponseUnit* sender, const std::string& event, const std::string& data) {
-	// TODO - implement CentralDispatchMediator::notify
-	throw "Not yet implemented";
+	std::string senderId = sender ? sender->getId() : "SYSTEM";
+ 
+    std::cout << "[CentralDispatchMediator] " << senderId
+              << " -> event: " << event << " (" << data << ")" << std::endl;
+ 
+    if (doors && (event == "ZONE_CORDONED" || event == "LOCKDOWN_TRIGGERED")) {
+        doors->lockZone(data);
+    } else if (doors && event == "EVACUATION_ORDERED") {
+        doors->unlockZone(data);
+    }
+ 
+    for (std::vector<ResponseUnit*>::iterator it = colleagues.begin(); it != colleagues.end(); ++it) {
+        ResponseUnit* colleague = *it;
+        if (colleague != sender) {
+            colleague->receiveNotification(senderId, event, data);
+        }
+    }
 }
