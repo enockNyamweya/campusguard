c8193b78c387b01d911fe866a6293c43eb12b059
Terrence <u25446968@tuks.co.za>
2026-09-26 18:47:04 +0200

    Implement ResolvedState methods with default behavior

--- src/ResolvedState.cpp
+++ src/ResolvedState.cpp
@@ -1,25 +1,7 @@
 #include "ResolvedState.h"
 
-ResolvedState::ResolvedState() {
-	// TODO - implement ResolvedState::ResolvedState
-}
-
-bool ResolvedState::handleDispatch(Incident* context) {
-	// TODO - implement ResolvedState::handleDispatch
-	throw "Not yet implemented";
-}
-
-bool ResolvedState::handleContain(Incident* context) {
-	// TODO - implement ResolvedState::handleContain
-	throw "Not yet implemented";
-}
-
-bool ResolvedState::handleResolve(Incident* context) {
-	// TODO - implement ResolvedState::handleResolve
-	throw "Not yet implemented";
-}
-
-std::string ResolvedState::getStateName() {
-	// TODO - implement ResolvedState::getStateName
-	throw "Not yet implemented";
-}
+ResolvedState::ResolvedState() = default;
+bool ResolvedState::handleDispatch(Incident*) { return false; }
+bool ResolvedState::handleContain(Incident*) { return false; }
+bool ResolvedState::handleResolve(Incident*) { return false; }
+std::string ResolvedState::getStateName() { return "Resolved"; }
